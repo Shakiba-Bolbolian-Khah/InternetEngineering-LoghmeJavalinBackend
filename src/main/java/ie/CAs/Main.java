7f03f948d4f4a11b1fe8f578ebb5ef1bbc05871c
Mir Hamed <h.jafar77@gmail.com>
2020-02-06 19:18:25 +0330

    parse in main - need editing

--- src/main/java/ie/CAs/Main.java
+++ src/main/java/ie/CAs/Main.java
@@ -1,9 +1,14 @@
 package ie.CAs;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import com.google.gson.*;
 
 public class Main {
-    public static void main(String[] args) throws java.io.IOException {
+    public static void main(String[] args) throws java.io.IOException, JsonParseException {
         BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while (true){
@@ -11,8 +16,16 @@ public class Main {
             if (line == null){
                 break;
             }
-            System.out.println(line);
-            System.out.println(line);
+//            System.out.println(line);
+            String[] parsedLine = line.split(" ",2);
+            String command = parsedLine[0];
+            String data = parsedLine[1];
+            Gson gson = new GsonBuilder().create();
+            Food food = gson.fromJson(data, Food.class);
+            System.out.println(food.getName());
+            System.out.println(food.getDescription());
+            System.out.println(food.getPopularity());
+            System.out.println(food.getPrice());
         }
     }
 }
