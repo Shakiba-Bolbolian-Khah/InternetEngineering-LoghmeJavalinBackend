974635db3587937e068fa0e01b6d71aeb9ec71da
Mir Hamed <h.jafar77@gmail.com>
2020-02-07 20:36:46 +0330

    interface done. working on commandHandler.

--- src/main/java/ie/CAs/CommandHandler.java
+++ src/main/java/ie/CAs/CommandHandler.java
@@ -1,9 +1,55 @@
 package ie.CAs;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class CommandHandler {
     Loghme loghme;
 
     public CommandHandler() {
         this.loghme = new Loghme();
+    }
+
+    public void addRestaurant(String newRestaurantInfo){
+        Gson gson = new GsonBuilder().create();
+        Restaurant newRestaurant = gson.fromJson(newRestaurantInfo, Restaurant.class);
+        System.out.println(loghme.addRestaurant(newRestaurant));
+    }
+
+    public void addFood(String newFoodInfo){
+        Gson gson = new GsonBuilder().create();
+        Food newFood = gson.fromJson(newFoodInfo, Food.class);
+        String restaurantName = new JsonParser().parse(newFoodInfo).getAsJsonObject().get("restaurantName").getAsString();
+        System.out.println(loghme.addFood(newFood, restaurantName));
+    }
+
+    public void getRestaurants(){
+
+    }
+
+    public void getRestaurant(String restaurantName){
+
+    }
+
+    public void getFood(String foodInfo){
+
+    }
+
+    public void addToCart(String foodInfo){
+
+    }
+
+    public void getCart(){
+
+    }
+
+    public void finalizeOrder(){
+
+    }
+
+    public void getRecommendedRestaurants(){
+
     }
 }
--- src/main/java/ie/CAs/Interface.java
+++ src/main/java/ie/CAs/Interface.java
@@ -1,10 +1,7 @@
 package ie.CAs;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParseException;
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Interface {
@@ -14,24 +11,58 @@ public class Interface {
         commandHandler = new CommandHandler();
     }
 
-    public static void main(String[] args) throws java.io.IOException, JsonParseException {
+    public static void main(String[] args) {
         BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
-        String line;
+        String line = "";
         while (true){
-            line = buff.readLine();
+            try {
+                line = buff.readLine();
+            } catch (IOException e) {
+                System.out.println("Error Wrong IO Command: Bad IO.");
+            }
             if (line == null){
                 break;
             }
-            String[] parsedLine = line.split(" ",2);
-            String command = parsedLine[0];
-            String data = parsedLine[1];
-            Gson gson = new GsonBuilder().create();
-            Food food = gson.fromJson(data, Food.class);
-            System.out.println(food.getName());
-            System.out.println(food.getDescription());
-            System.out.println(food.getPopularity());
-            System.out.println(food.getPrice());
+            String command, data = "";
+            try {
+                String[] parsedLine = line.split("\\s", 2);
+                command = parsedLine[0];
+                data = parsedLine[1];
+            } catch (ArrayIndexOutOfBoundsException e){
+                command = line;
+            }
+            Interface interFace = new Interface();
+            switch (command) {
+                case "addRestaurant":
+                    interFace.commandHandler.addRestaurant(data);
+                    break;
+                case "addFood":
+                    interFace.commandHandler.addFood(data);
+                    break;
+                case "getRestaurants":
+                    interFace.commandHandler.getRestaurants();
+                    break;
+                case "getRestaurant":
+                    interFace.commandHandler.getRestaurant(data);
+                    break;
+                case "getFood":
+                    interFace.commandHandler.getFood(data);
+                    break;
+                case "addToCart":
+                    interFace.commandHandler.addToCart(data);
+                    break;
+                case "getCart":
+                    interFace.commandHandler.getCart();
+                    break;
+                case "finalizeOrder":
+                    interFace.commandHandler.finalizeOrder();
+                    break;
+                case "getRecommendedRestaurants":
+                    interFace.commandHandler.getRecommendedRestaurants();
+                    break;
+                default:
+                    System.out.println("Error Wrong IO Command: Command is invalid.");
+            }
         }
     }
 }
-
