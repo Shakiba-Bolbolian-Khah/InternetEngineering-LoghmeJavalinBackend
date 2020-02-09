c00a4f15195e7f45ad629a79449a90acba989761
Mir Hamed <h.jafar77@gmail.com>
2020-02-09 17:45:10 +0330

    Phase1 Finished.

--- src/main/java/ie/CAs/Interface.java
+++ src/main/java/ie/CAs/Interface.java
@@ -3,7 +3,6 @@ package ie.CAs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Interface {
     CommandHandler commandHandler;
@@ -16,89 +15,54 @@ public class Interface {
         Interface interFace = new Interface();
         BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
         String line = "";
-//        while (true){
-//            try {
-//                line = buff.readLine();
-//            } catch (IOException e) {
-//                System.out.println("Error Wrong IO Command: Bad IO.");
-//            }
-//            if (line == null || line.equals("exit")){
-//                break;
-//            }
-//            String command, data = "";
-//            try {
-//                String[] parsedLine = line.split("\\s", 2);
-//                command = parsedLine[0];
-//                data = parsedLine[1];
-//            } catch (ArrayIndexOutOfBoundsException e){
-//                command = line;
-//            }
-//            switch (command) {
-//                case "addRestaurant":
-//                    interFace.commandHandler.addRestaurant(data);
-//                    break;
-//                case "addFood":
-//                    interFace.commandHandler.addFood(data);
-//                    break;
-//                case "getRestaurants":
-//                    interFace.commandHandler.getRestaurants();
-//                    break;
-//                case "getRestaurant":
-//                    interFace.commandHandler.getRestaurant(data);
-//                    break;
-//                case "getFood":
-//                    interFace.commandHandler.getFood(data);
-//                    break;
-//                case "addToCart":
-//                    interFace.commandHandler.addToCart(data);
-//                    break;
-//                case "getCart":
-//                    interFace.commandHandler.getCart();
-//                    break;
-//                case "finalizeOrder":
-//                    interFace.commandHandler.finalizeOrder();
-//                    break;
-//                case "getRecommendedRestaurants":
-//                    interFace.commandHandler.getRecommendedRestaurants();
-//                    break;
-//                default:
-//                    System.out.println("Error Wrong IO Command: Command is invalid.");
-//            }
-//        }
-        Loghme l = new Loghme();
-        ArrayList<Food> menu = new ArrayList<Food>();
-        ArrayList<Food> menu1 = new ArrayList<Food>();
-        ArrayList<Food> menu2 = new ArrayList<Food>();
-        ArrayList<Food> menu3 = new ArrayList<Food>();
-        Food f = new Food("f","2",3,4);
-        Food f1 = new Food("f1","3",6,6);
-        menu.add(f);
-        System.out.println("Size"+menu.size());
-        Restaurant r1 = new Restaurant("r1","b",new Location(1,2),menu);
-        System.out.println(r1.addFood(f1));
-        System.out.println("Size"+menu.size());
-        System.out.println(l.addRestaurant(r1));
-//        menu1.add(f1);
-        Restaurant r2 = new Restaurant("r2","d",new Location(3,4),menu1);
-        System.out.println(l.addRestaurant(r2));
-        Food f3 = new Food("f3","3",688,65);
-        Restaurant r3 = new Restaurant("r3","d",new Location(3,4),menu2);
-        System.out.println(r3.addFood(f3));
-        System.out.println("Size"+menu.size());
-        System.out.println(r3.addFood(f1));
-        System.out.println(l.addRestaurant(r3));
-        Restaurant r4 = new Restaurant("r4","d",new Location(3,4),menu3);
-        System.out.println(r4.addFood(f3));
-        System.out.println(r4.addFood(f1));
-        System.out.println(l.addRestaurant(r4));
-        System.out.println(l.addToCart("r1","f1"));
-        System.out.println(l.addToCart("r1","f1"));
-        System.out.println(l.addToCart("r1","f"));
-        System.out.println(l.getRecommendedRestaurants());
-        System.out.println(l.getCart());
-//        System.out.println(l.getRestaurant("a"));
-        System.out.println(l.finalizeOrder());
-
-
+        while (true){
+            try {
+                line = buff.readLine();
+            } catch (IOException e) {
+                System.out.println("Error Wrong IO Command: Bad IO.");
+            }
+            if (line == null || line.equals("exit")){
+                break;
+            }
+            String command, data = "";
+            try {
+                String[] parsedLine = line.split("\\s", 2);
+                command = parsedLine[0];
+                data = parsedLine[1];
+            } catch (ArrayIndexOutOfBoundsException e){
+                command = line;
+            }
+            switch (command) {
+                case "addRestaurant":
+                    interFace.commandHandler.addRestaurant(data);
+                    break;
+                case "addFood":
+                    interFace.commandHandler.addFood(data);
+                    break;
+                case "getRestaurants":
+                    interFace.commandHandler.getRestaurants();
+                    break;
+                case "getRestaurant":
+                    interFace.commandHandler.getRestaurant(data);
+                    break;
+                case "getFood":
+                    interFace.commandHandler.getFood(data);
+                    break;
+                case "addToCart":
+                    interFace.commandHandler.addToCart(data);
+                    break;
+                case "getCart":
+                    interFace.commandHandler.getCart();
+                    break;
+                case "finalizeOrder":
+                    interFace.commandHandler.finalizeOrder();
+                    break;
+                case "getRecommendedRestaurants":
+                    interFace.commandHandler.getRecommendedRestaurants();
+                    break;
+                default:
+                    System.out.println("Error Wrong IO Command: Command is invalid.");
+            }
+        }
     }
 }
--- src/test/java/ie/CAs/LoghmeTest.java
+++ src/test/java/ie/CAs/LoghmeTest.java
@@ -3,8 +3,11 @@ package ie.CAs;
 import org.junit.*;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LoghmeTest {
     CommandHandler commandHandler;
@@ -21,6 +24,17 @@ public class LoghmeTest {
                 break;
             }
             commandHandler.addRestaurant(restaurantInfo);
+        }
+    }
+
+    public void setFoodsForFinalizingOrderTest(int num) throws IOException {
+        BufferedReader buff = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/testFinalizeOrder" + num + ".txt")));
+        while (true){
+            String foodInfo = buff.readLine();
+            if (foodInfo == null){
+                break;
+            }
+            commandHandler.addToCart(foodInfo);
         }
     }
 
@@ -76,37 +90,45 @@ public class LoghmeTest {
     }
 
     @Test
-    public void testFinalize0() throws IOException {
+    public void testFinalize1() throws IOException {
         setRestaurantsForFinalizingOrderTest();
         String expected = "Error: There is nothing to be finalized in your cart!";
         assertEquals(commandHandler.getLoghme().finalizeOrder(),expected);
     }
 
     @Test
-    public void testFinalize1() throws IOException {
-        setRestaurantsForFinalizingOrderTest();
-        String expected = "";
-        assertEquals(commandHandler.getLoghme().getRecommendedRestaurants(),expected);
-    }
-
-    @Test
     public void testFinalize2() throws IOException {
         setRestaurantsForFinalizingOrderTest();
-        String expected = "";
-        assertEquals(commandHandler.getLoghme().getRecommendedRestaurants(),expected);
+        setFoodsForFinalizingOrderTest(2);
+        String expected = new String(Files.readAllBytes(Paths.get("src/test/resources/testFinalizeOut2.txt")));
+        assertEquals(removeWhiteSpaces(commandHandler.getLoghme().finalizeOrder()),removeWhiteSpaces(expected));
     }
 
     @Test
     public void testFinalize3() throws IOException {
         setRestaurantsForFinalizingOrderTest();
-        String expected = "";
-        assertEquals(commandHandler.getLoghme().getRecommendedRestaurants(),expected);
+        setFoodsForFinalizingOrderTest(3);
+        String expected = new String(Files.readAllBytes(Paths.get("src/test/resources/testFinalizeOut3.txt")));
+        assertEquals(removeWhiteSpaces(commandHandler.getLoghme().finalizeOrder()),removeWhiteSpaces(expected));
+    }
+
+    @Test
+    public void testFinalize4() throws IOException {
+        setRestaurantsForFinalizingOrderTest();
+        setFoodsForFinalizingOrderTest(4);
+        String output = removeWhiteSpaces(commandHandler.getLoghme().finalizeOrder());
+        assertTrue(output.contains("\"GhormeSabzi\":1"));
+        assertTrue(output.contains("\"Kebab\":1"));
+        assertTrue(output.contains("\"Gheime\":1"));
     }
 
     @Test
     public void testFinalizeAll() throws IOException {
         setRestaurantsForFinalizingOrderTest();
-        String expected = "";
-        assertEquals(commandHandler.getLoghme().getRecommendedRestaurants(),expected);
+        setFoodsForFinalizingOrderTest(5);
+        String output = removeWhiteSpaces(commandHandler.getLoghme().finalizeOrder());
+        assertTrue(output.contains("\"Fesenjoon\":2"));
+        assertTrue(output.contains("\"TahChin\":4"));
+        assertTrue(output.contains("\"ZereshkPolo\":3"));
     }
 }
